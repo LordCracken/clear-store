88ae4ed1c89489756b6f1d5a32d3716ed566f77b
LordCracken <vladislav070201@gmail.com>
2023-02-06 15:07:12 +0300

    add calculating product price and total price in Cart entity

--- src/entities/cart.ts
+++ src/entities/cart.ts
@@ -1,47 +1,64 @@
-interface CartItem {
-  productId: UniqueID;
+import Product from './product';
+
+interface CartItem extends Pick<Product, 'id' | 'price'> {
   quantity: number;
 }
 
 class Cart {
   products: CartItem[] = [];
+  totalPrice = 0;
 
-  addProduct(productId: UniqueID) {
-    const existingItemIndex = this.products.findIndex(item => item.productId === productId);
+  addProduct(product: Product) {
+    const existingItemIndex = this.products.findIndex(item => item.id === product.id);
     const existingItem = this.products.at(existingItemIndex);
     let updatedCart;
 
     if (existingItem) {
-      const updatedItem = { ...existingItem, quantity: existingItem.quantity + 1 };
+      const updatedItem = {
+        ...existingItem,
+        price: existingItem.price + product.price,
+        quantity: existingItem.quantity + 1,
+      };
       updatedCart = [...this.products];
       updatedCart[existingItemIndex] = updatedItem;
     } else {
-      updatedCart = this.products.concat({ productId, quantity: 1 });
+      updatedCart = this.products.concat({ id: product.id, price: product.price, quantity: 1 });
     }
+
+    this.totalPrice += product.price;
 
     return updatedCart;
   }
 
   removeProduct(productId: UniqueID) {
-    const existingItemIndex = this.products.findIndex(item => item.productId === productId);
+    const existingItemIndex = this.products.findIndex(item => item.id === productId);
     const existingItem = this.products.at(existingItemIndex);
-    let updatedCart;
 
     if (!existingItem) return;
 
+    let updatedCart;
+    const productPrice = existingItem.price / existingItem.quantity;
+
     if (existingItem.quantity === 1) {
-      updatedCart = this.products.filter(item => item.productId !== productId);
+      updatedCart = this.products.filter(item => item.id !== productId);
     } else {
-      const updatedItem = { ...existingItem, quantity: existingItem.quantity - 1 };
+      const updatedItem = {
+        ...existingItem,
+        price: existingItem.price - productPrice,
+        quantity: existingItem.quantity - 1,
+      };
       updatedCart = [...this.products];
       updatedCart[existingItemIndex] = updatedItem;
     }
+
+    this.totalPrice -= productPrice;
 
     return updatedCart;
   }
 
   empty() {
     this.products = [];
+    this.totalPrice = 0;
   }
 }
 
