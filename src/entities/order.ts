874caee4905a5f5c25d3c27e30c3bada0fc2b4a3
LordCracken <vladislav070201@gmail.com>
2023-02-06 03:05:09 +0300

    add start order time and complete order time in Order entity

--- src/entities/order.ts
+++ src/entities/order.ts
@@ -9,16 +9,32 @@ class Order {
   id: UniqueID;
   user: UniqueID;
   cart: Cart;
+  startTime: DateTime;
+  completeTime: DateTime = '';
   status: OrderStatuses = OrderStatuses.New;
+  private dateOptions: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+  };
 
   constructor(id: UniqueID, user: UniqueID, cart: Cart) {
     this.id = id;
     this.user = user;
     this.cart = cart;
+
+    const date = new Date();
+    this.startTime = date.toLocaleString('ru', this.dateOptions);
   }
 
   finishOrder() {
     this.status = OrderStatuses.Completed;
+
+    const date = new Date();
+    this.completeTime = date.toLocaleString('ru', this.dateOptions);
   }
 }
 
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -3,3 +3,4 @@ declare module '*.module.scss';
 type UniqueID = string;
 type Email = string;
 type ImageURL = string;
+type DateTime = string;
