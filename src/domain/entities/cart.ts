2d06b92acbe189b3bfedf34e52fec50722f872de
LordCracken <vladislav070201@gmail.com>
2023-02-13 14:19:18 +0300

    transform Cart class into interface

--- src/domain/entities/cart.ts
+++ src/domain/entities/cart.ts
@@ -4,14 +4,9 @@ export interface CartItem extends Pick<Product, 'id' | 'price'> {
   quantity: number;
 }
 
-class Cart {
-  products: CartItem[] = [];
+interface Cart {
+  products: CartItem[];
   totalPrice: number;
-
-  constructor(products: CartItem[], totalPrice: number) {
-    this.products = products;
-    this.totalPrice = totalPrice;
-  }
 }
 
 export default Cart;
