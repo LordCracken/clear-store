8b7cf690326a86f4b68213718b454c3287433828
LordCracken <vladislav070201@gmail.com>
2023-03-07 13:30:55 +0300

    add logic for adding and removing product in cart entity

--- src/domain/entities/cart.ts
+++ src/domain/entities/cart.ts
@@ -1,4 +1,4 @@
-import { IProduct } from './product';
+import {IProduct} from './product';
 
 export interface CartItem extends Pick<IProduct, 'id' | 'price'> {
   quantity: number;
@@ -10,6 +10,35 @@ interface ICart {
 }
 
 export class Cart implements ICart {
-  products = [];
+  products: CartItem[] = [];
   totalPrice = 0;
+
+  addProduct(id: UniqueID, price: number) {
+    const existingItemIndex = this.products.findIndex(item => item.id === id);
+
+    if (existingItemIndex) {
+      this.products[existingItemIndex].price += price;
+      this.products[existingItemIndex].quantity += 1;
+    } else {
+      this.products.concat({ id, price, quantity: 1 });
+    }
+
+    this.totalPrice += price;
+  }
+
+  removeProduct(productId: UniqueID, price: number) {
+    const existingItemIndex = this.products.findIndex(item => item.id === productId);
+    const existingItem = this.products.at(existingItemIndex);
+
+    if (!existingItem) return;
+
+    if (existingItem.quantity === 1) {
+      this.products = this.products.filter(item => item.id !== productId);
+    } else {
+      this.products[existingItemIndex].price -= price;
+      this.products[existingItemIndex].quantity -= 1;
+    }
+
+    this.totalPrice -= price;
+  }
 }
