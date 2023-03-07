9abd16ca9e01f19bd748e69b99633fa183730eea
LordCracken <vladislav070201@gmail.com>
2023-03-07 16:48:44 +0300

    initialize firebase app in adapters layer

---
+++ src/adapters/gateways/index.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCPk9eSuA26tsyqIVeQvNUO2dr7F1i6trE',
+  authDomain: 'clean-store-e7a58.firebaseapp.com',
+  projectId: 'clean-store-e7a58',
+  storageBucket: 'clean-store-e7a58.appspot.com',
+  messagingSenderId: '1046805804029',
+  appId: '1:1046805804029:web:29819fd63669582ae71f94',
+};
+
+initializeApp(firebaseConfig);
