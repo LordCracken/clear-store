f45ee25996756b2d993bd6817e72ad55191944dc
LordCracken <vladislav070201@gmail.com>
2023-03-07 16:40:46 +0300

    remove uuid and web-vitals packages

--- src/domain/lib/generateId.ts
+++
@@ -1,5 +0,0 @@
-import { v4 } from 'uuid';
-
-const generateId = () => v4();
-
-export default generateId;
--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,8 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './infrastructure/App';
 
-import store from './infrastructure/store';
+import store from './adapters/redux';
 import './index.scss';
-
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
@@ -20,8 +18,3 @@ root.render(
     </Provider>
   </React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
