8b77b92a5eec66717228a1c66403f05c6597fd08
LordCracken <vladislav070201@gmail.com>
2023-03-09 12:39:53 +0300

    fix user slice selectors

--- src/adapters/redux/user/selectors.ts
+++ src/adapters/redux/user/selectors.ts
@@ -1,3 +1,1 @@
-export const selectUser = (state: RootState) => state.user.user;
-
-export const selectUserEmail = (state: RootState) => state.user.user.email;
+export const selectIsAuth = (state: RootState) => state.user.isAuthenticated;
