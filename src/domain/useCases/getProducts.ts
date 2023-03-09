62c71b4281a930b9a55228364d4f65034ae93386
LordCracken <vladislav070201@gmail.com>
2023-03-09 13:23:38 +0300

    add get products use case

---
+++ src/domain/useCases/getProducts.ts
@@ -0,0 +1,17 @@
+import { Product } from '../entities';
+
+export interface GetProductsService {
+  getProducts: () => Promise<Product[]>;
+}
+
+export class GetProductsCase {
+  getProductsService: GetProductsService;
+
+  constructor(service: GetProductsService) {
+    this.getProductsService = service;
+  }
+
+  async getProducts() {
+    return this.getProductsService.getProducts();
+  }
+}
