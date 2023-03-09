17af4c859b9315f231b122812748efac4a4ee09a
LordCracken <vladislav070201@gmail.com>
2023-03-09 18:30:26 +0300

    add cart service class

---
+++ src/adapters/gateways/cart.ts
@@ -0,0 +1,45 @@
+import { AuthService } from './';
+import { Cart } from '../../domain/entities';
+import { AddToCartService, EmptyCartService, RemoveFromCartService } from '../../domain/useCases';
+import { CartData } from '../../domain/entities/DTO';
+
+export class CartService
+  extends AuthService
+  implements AddToCartService, RemoveFromCartService, EmptyCartService
+{
+  private readonly url = `${this.baseUrl}/${this.uid}/cart.json`;
+
+  constructor() {
+    super();
+  }
+
+  async add(id: UniqueID, cartData: CartData) {
+    const cart = new Cart(cartData.products);
+    cart.addProduct(id);
+
+    const response = await fetch(this.url, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(new CartData(cart)),
+    });
+
+    return response.json();
+  }
+
+  async remove(id: UniqueID, cartData: CartData) {
+    const cart = new Cart(cartData.products);
+    cart.removeProduct(id);
+
+    const response = await fetch(this.url, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(new CartData(cart)),
+    });
+
+    return response.json();
+  }
+
+  async empty() {
+    await fetch(this.url, { method: 'DELETE' });
+  }
+}
