e99633c7c1dc9501834caa78375ec8ae9d337504
LordCracken <vladislav070201@gmail.com>
2023-03-10 15:33:00 +0300

    add set cart and reset cart reducers in redux store

--- src/adapters/redux/cart/slice.ts
+++ src/adapters/redux/cart/slice.ts
@@ -2,6 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 import { CartData } from '../../../domain/entities/DTO';
 
 interface ICart extends CartData {
+  totalPrice: number;
   isOpen: boolean;
 }
 
@@ -15,6 +16,11 @@ const Slice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
+    setCart(state, action) {
+      state.products = action.payload.products;
+      state.totalPrice = action.payload.totalPrice;
+    },
+    reset: () => initialState,
     openCart(state) {
       state.isOpen = true;
     },
