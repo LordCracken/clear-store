492986ae062591e341dbf18a4cd56376d120f8e6
LordCracken <vladislav070201@gmail.com>
2023-03-12 20:56:32 +0300

    implement autologin and auto getting cart logic in app interface

--- src/infrastructure/App.tsx
+++ src/infrastructure/App.tsx
@@ -1,4 +1,6 @@
+import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 // MUI
 import { Container } from '@mui/material';
 // Components
@@ -7,8 +9,25 @@ import AuthPage from './pages/AuthPage';
 import ProductsPage from './pages/ProductsPage';
 import ProfilePage from './pages/ProfilePage';
 import Cart from './components/Cart/Cart';
+import { useAppDispatch, useAppSelector } from './hooks';
+import { autologin, selectIsAuth } from '../adapters/redux/user';
+import { getCart } from '../adapters/redux/cart/actions';
 
 const App = () => {
+  const auth = getAuth();
+  const dispatch = useAppDispatch();
+  const isAuth = useAppSelector(selectIsAuth);
+
+  useEffect(() => {
+    return onAuthStateChanged(auth, user => {
+      if (user) dispatch(autologin());
+    })();
+  }, []);
+
+  useEffect(() => {
+    dispatch(getCart());
+  }, [isAuth]);
+
   return (
     <>
       <Header />
