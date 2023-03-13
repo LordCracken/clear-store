0b92f73ee42400bacd213580fd69f1f7e81461fb
LordCracken <vladislav070201@gmail.com>
2023-03-13 04:03:44 +0300

    remove unused description property from product entity

--- src/domain/entities/product.ts
+++ src/domain/entities/product.ts
@@ -3,7 +3,6 @@ export interface IProduct {
   readonly name: string;
   readonly author: string;
   readonly image: ImageURL;
-  readonly description: string;
   readonly price: number;
 }
 
@@ -12,22 +11,13 @@ export class Product implements IProduct {
   name;
   author;
   image;
-  description;
   price;
 
-  constructor(
-    id: UniqueID,
-    name: string,
-    author: string,
-    image: ImageURL,
-    description: string,
-    price: number,
-  ) {
+  constructor(id: UniqueID, name: string, author: string, image: ImageURL, price: number) {
     this.id = id;
     this.name = name;
     this.author = author;
     this.image = image;
-    this.description = description;
     this.price = price;
   }
 }
